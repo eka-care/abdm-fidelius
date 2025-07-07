4cdfa7c59d2c73e137e4e4f835b57269a4187795
Shyamjith Palakkandy <shyamjith.palakkandy@eka.care>
2025-07-07 20:13:23 +0530

    revert

--- src/main/java/in/projecteka/fidelius/FideliusApplication.java
+++ src/main/java/in/projecteka/fidelius/FideliusApplication.java
@@ -2,17 +2,9 @@ package in.projecteka.fidelius;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import java.security.Security;
 
 @SpringBootApplication
 public class FideliusApplication {
-
-    static {
-        if (Security.getProvider("BC") == null) {
-            Security.addProvider(new BouncyCastleProvider());
-        }
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(FideliusApplication.class, args);
